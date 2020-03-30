056877d9763bd831fc275dbc03597a733f00c727
Morre Thierry <Briareos@live.be>
2020-03-30 10:04:03 +0200

    01 Solution à l'héritage multiple

---
+++ DemoObserverV1/ObserverPattern/Subject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoObserver.ObserverPattern
+{
+    public abstract class Subject : ISubject
+    {
+        private List<IObserver> _observers;
+
+        public Subject()
+        {
+            _observers = new List<IObserver>();
+        }
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        protected void Notify()
+        {
+            foreach (IObserver observer in _observers)
+            {                
+                observer.Update(this);
+            }
+        }
+    }
+}
---
+++ DemoObserverV1/Program.cs
@@ -0,0 +1,21 @@
+﻿using DemoObserver.Models;
+using System;
+
+namespace DemoObserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acheteur lukas = new Acheteur("Lukas");
+            Acheteur charlotte = new Acheteur("Charlotte");
+            Produit coca33CL = new Produit("Coca 33cl");
+
+            coca33CL.Attach(lukas);
+            coca33CL.Attach(charlotte);
+
+            coca33CL.Prix = .80;
+
+        }
+    }
+}
